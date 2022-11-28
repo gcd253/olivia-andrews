8216194c01768be660b4ded2d34feeb1d4032163
Griffin Dooley <dooleygriffin@gmail.com>
2022-11-28 13:25:31 -0500

    init

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Footer, Navbar } from '../components';
+
+const Page = () => (
+  <div>
+    <Navbar />
+
+    <Footer />
+  </div>
+);
+
+export default Page;
---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => (
+  <footer className="justify-center flex">
+    <div className="border-t border-slate-300 w-[90%] flex flex-col items-center space-y-6 text-slate-400">
+      <div className="pt-8 hover:text-slate-500 hover:cursor-pointer duration-250 hover:scale-[1.02] hover:my-1 transition-all font-medium">CONTACT</div>
+      <div className="hover:text-slate-500 hover:cursor-pointer duration-300 hover:scale-[1.02] hover:py-1 transition-all font-medium">GALLERIES</div>
+      <div className="hover:text-slate-500 hover:cursor-pointer duration-300 hover:scale-[1.02] hover:py-1 transition-all font-medium">OTHER</div>
+    </div>   
+  </footer>
+);
+
+export default Footer;
